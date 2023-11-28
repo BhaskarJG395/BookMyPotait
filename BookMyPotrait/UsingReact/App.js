ce967eb0d42657e98ef6a0440f844f2f56b5fc2c
BhaskarJG395 <106325572+BhaskarJG395@users.noreply.github.com>
2023-11-28 23:18:03 +0530

    Add files via upload
    Now restarting the project with React JS . Bringing the files by using (create-react-app UsingReact) command.

---
+++ BookMyPotrait/UsingReact/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NavigationBar from './components/Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
