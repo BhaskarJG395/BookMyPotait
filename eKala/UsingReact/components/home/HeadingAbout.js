7f4c9970533c58749082e405074ae731d4a2ca68
BhaskarJG395 <bjyotig2018@gmail.com>
2024-02-10 11:47:26 +0530

    chainged name from BookMyPortait from eKala

---
+++ eKala/UsingReact/components/home/HeadingAbout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ArtistBook from './ArtistBook'
+import UserBook from './UserBook'
+import ArtShowcase from './ArtShowcase'
+
+export default function HeadingAbout(){
+    return(
+        <div>        
+            <div>
+                {/*Artist Book box will be here*/}
+                {/*User book will be here*/}
+                {/*Art showcase will be here*/}
+                <p>About this page will be here.</p>
+            </div>
+        </div>
+
+    )
+}
