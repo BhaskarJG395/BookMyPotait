16f6e3474410313af6e0a0ed959f5cab9edcd05d
BhaskarJG395 <bjyotig2018@gmail.com>
2024-02-15 22:10:49 +0530

    artistBook is modified

--- eKala/UsingReact/src/components/home/ArtistBook.js
+++ eKala/UsingReact/src/components/home/ArtistBook.js
@@ -1,4 +1,5 @@
 import art1 from "../../image/art1.png"
+import { NavLink } from "react-router-dom"
 export default function ArtistBook(){
     const imageStyle={
         height:'42em',
@@ -14,14 +15,17 @@ export default function ArtistBook(){
                         <h1 className="p-3"><strong>Discover the beauty of artist expression</strong></h1>
                         <p className="p-2">Immagine yourself in the world of creativity and inspiration,where artists showcase their unique talents and visions.</p>
                         <div className="p-2">
-                            <button type="button" className="btn btn-success p-2.5 btn-lg">Join as an Artist</button>
+                            <button type="button" className="btn btn-dark p-2.5 btn-lg">Explore</button> {' '}
+                            <NavLink to="/login">
+                                <button type="button" className="btn btn-success p-2.5 btn-lg">Join as an Artist</button>
+                            </NavLink>
                         </div>
                     </div>
                     <div className="col col-5 p-6 m-4">
-			<p>an image will be here</p>
+                        <img src={art1} style={imageStyle} />
                     </div>
                 </div>
             </div>
         </div>
-     )
+    )
 }
