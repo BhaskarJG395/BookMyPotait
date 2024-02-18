9e3e6ac607c29ab938d5d817e6106a3755e2260f
BhaskarJG395 <bjyotig2018@gmail.com>
2024-02-18 11:00:04 +0530

    import few pictures inside artShowcase and applied css and bootstrap to look as grid

--- eKala/UsingReact/src/components/home/ArtShowcase.js
+++ eKala/UsingReact/src/components/home/ArtShowcase.js
@@ -3,8 +3,31 @@ import {Pagination } from "react-bootstrap"
 import {NavLink} from "react-router-dom"
 
 export default function ArtShowcase(){
-    return(
+        return(
         <div className='bg-img'>
+        <hr/>
+            <h1 className="text-center"><strong>Art Gallary</strong></h1>
+            <div className="container text-center">
+                <div className="row artShowcaseRow">
+                    <div className="col artShowcaseCol">
+                        <img src={drawing1}></img>
+                        <img src={drawing2}></img>
+                    </div>
+                    <div className="col artShowcaseCol">
+                        <img src={drawing3}></img>
+                        <img src={drawing4}></img>
+                    </div>
+                    <div className="col artShowcaseCol">
+                        <img src={drawing5}></img>
+                        <img src={drawing6}></img>
+                    </div>
+                </div>
+            </div>
+            <div className="text-center">
+                    <NavLink to="/gallery"><button type="button" className="btn btn-dark">......................Explore More.....................</button></NavLink>
+            </div>
+            <hr/>
         </div>
     )
+
 }
